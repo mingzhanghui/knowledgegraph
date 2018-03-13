60bd0d300e7e9e778b528ad60bd23d190acc1b63
mingzhanghui <mingzh_tflx@tsinghuadtv.com>
2018-03-13 19:21:29 +0800

    test xhr interact with php

--- js/web/structMap.js
+++ js/web/structMap.js
@@ -24,7 +24,11 @@ function loadMap(t, a, n) {
           o = Math.sqrt(l * l + i * i),
           s = l / o,
           u = i / o;
-        e = t.source.x + 35 * s, r = t.target.x - 35 * s, a = t.source.y + 35 * u, n = t.target.y - 35 * u, c.attr("marker-end", "url(#arrow)")
+        e = t.source.x + 35 * s,
+        r = t.target.x - 35 * s,
+        a = t.source.y + 35 * u,
+        n = t.target.y - 35 * u,
+        c.attr("marker-end", "url(#arrow)")
       } else e = t.source.x, a = t.source.y, r = t.target.x, n = t.target.y;
       c.attr("x1", e), c.attr("x2", r), c.attr("y1", a), c.attr("y2", n)
     }), s.selectAll("g.node").selectAll("circle.ring").attr({
@@ -68,14 +72,17 @@ function loadMap(t, a, n) {
         return "rotate(" + 360 * Math.atan(r / e) / (2 * Math.PI) + "," + (t.target.x + t.source.x) / 2 + "," + (t.target.y + t.source.y) / 2 + ")"
       }
     })
-  }).charge(-1300).linkDistance(200).nodes(a).links(n).size([l, i]).alpha(.1), o.append("svg:defs").append("svg:marker").attr("id", "arrow").attr("viewBox", "0 -5 10 10").attr("refX", 6).attr("markerWidth", 5).attr("markerHeight", 5).attr("orient", "auto").append("svg:path").attr("d", "M0,-5L10,0L0,5").attr("fill", "#6ac6ff"), s.selectAll("line.link").data(n).enter().append("line").attr("class", "link");
+  }).charge(-1300).linkDistance(200).nodes(a).links(n).size([l, i]).alpha(.1),
+      o.append("svg:defs").append("svg:marker").attr("id", "arrow").attr("viewBox", "0 -5 10 10").attr("refX", 6).attr("markerWidth", 5).attr("markerHeight", 5).attr("orient", "auto").append("svg:path").attr("d", "M0,-5L10,0L0,5").attr("fill", "#6ac6ff"),
+      s.selectAll("line.link").data(n).enter().append("line").attr("class", "link");
 
   var d = s.selectAll("link.desc").data(n).enter().append("text").attr("class", "desc").text(function(t) {
       return t.desc
     }),
-  f = (u.drag().on("dragstart", function(t) {
+    f = (u.drag().on("dragstart", function(t) {
       t.fixed = !0
-    }), s.selectAll("g.node").data(a)),
+    }),
+   s.selectAll("g.node").data(a)),
     p = f.enter().append("g").attr("class", function(t, e) {
       return 0 === e ? "node active" : "node"
     }).call(u.drag).on("click", function(t) {

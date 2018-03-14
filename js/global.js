942d2ab236e2439792c9b607733aa0bace304626
mingzhanghui <1335250574@qq.com>
2018-03-14 23:57:05 +0800

    works for chart with database contains only one structure (e.g. PHP), nText is TBD

--- js/global.js
+++ js/global.js
@@ -1,1 +1,2 @@
-window.CONTEXT_PATH = "http://localhost:8000/d3.js/graph/public/index.php";
+// window.CONTEXT_PATH = "http://localhost:8000/d3.js/graph/public/index.php";
+window.CONTEXT_PATH = "http://localhost/d3.js/graph/public/index.php";

40e45bf6b855be2551c8ae27fe18d81246c9d8ac
mingzhanghui <1335250574@qq.com>
2018-03-26 23:37:03 +0800

    知识结构树形图3层加载 折叠事件还没有+

--- js/web/structure.js
+++ js/web/structure.js
@@ -17,8 +17,48 @@ $(function() {
         type: 'GET',
         url: window.CONTEXT_PATH + "/Index/nodeTree",
         data: {structid:structid}
-    }).done(function(data) {
-        console.log(data);
+    }).done(function(resp) {
+        var data = resp[0];
+
+        var $menu = $("#J_menu").empty();
+        var $box = $("<ul>").addClass("menubox").addClass("treeboxnew");
+        $menu.append($box);
+        // depth = 1;
+        data.childs && data.childs.forEach(function(a) {
+            var $li = $("<li>").attr("data-id", a.id);
+            $box.append($li);
+            var $whiteblank = $("<span>").addClass("whiteblank").html("<em class=\"cirle\"></em>");
+            $li.append($whiteblank);
+            var $a = $("<a>").attr("href", a.href).attr("data-id", a.id);
+            var $em = $("<em>").text(a.name);
+            var $i = a.childs ? $("<i>").attr("class", "fa iconfa fa-angle-down") : null;
+            $a.append($em).append($i);
+            $li.append($a);
+
+            // depth = 2
+            var $subul;
+            a.childs && ($subul = $("<ul>").addClass("subul").appendTo($li).show()) && a.childs.forEach(function(b) {
+                var $subli = $("<li>").attr("data-id", b.id);
+                $subul.append($subli);
+                var $a = $("<a>").attr("data-id", b.id);  // addClass("roots") highlight
+                $subli.append($a);
+                $em = $("<em>").addClass("iplus").appendTo($a);
+                var $i = b.childs ? $("<i>").attr("class", "fa iconfa fa-angle-down") : null;
+                $("<span>").text(b.name).append($i).appendTo($a);
+
+                // depth = 3
+                var $leaful;
+                b.childs && ($leaful = $("<ul>").addClass("subul").appendTo($subli).show()) && b.childs.forEach(function(c) {
+                    var $leafli = $("<li>").attr("data-id", c.id);
+                    $leaful.append($leafli);
+                    var $a = $("<a>").attr("data-id", c.id).attr("href", c.href);
+                    $("<em>").addClass("iless").appendTo($a);
+                    $("<span>").text(c.name).appendTo($a);
+                    $leafli.append($a);
+                });
+            });
+        });
+        // end data.childs.forEach
     })
 });
 

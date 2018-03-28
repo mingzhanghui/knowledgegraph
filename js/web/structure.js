c7ec6aee6dfbbdf1db2890f0da97f8c1edb2ca07
mingzhanghui <mingzh_tflx@tsinghuadtv.com>
2018-03-28 17:26:54 +0800

    树形结构折叠/收起

--- js/web/structure.js
+++ js/web/structure.js
@@ -4,9 +4,9 @@
 var structid = null;
 
 $(function() {
-    structid = UrlParam.hasParam('structid') ? UrlParam.param("structid") : 1;  // default value for test
+    structid = UrlParam.hasParam('structid') ? UrlParam.param("structid") : 1; // default value for test
 
-    $("#J_preview").on("click", function (e) {
+    $("#J_preview").on("click", function(e) {
         e.preventDefault();
         // chart.html?structid=1
         location.href = this.href + "?structid=" + structid;
@@ -16,7 +16,7 @@ $(function() {
     $.ajax({
         type: 'GET',
         url: window.CONTEXT_PATH + "/Index/nodeTree",
-        data: {structid:structid}
+        data: { structid: structid }
     }).done(function(resp) {
         var data = resp[0];
 
@@ -32,23 +32,50 @@ $(function() {
             var $a = $("<a>").attr("href", a.href).attr("data-id", a.id);
             var $em = $("<em>").text(a.name);
             var $i = a.childs ? $("<i>").attr("class", "fa iconfa fa-angle-down") : null;
+            // toggle hide/show
+            $i && $i.on("click", function(e) {
+                e.preventDefault();
+                var t = this,
+                    a = t.parentNode;
+                if (this.classList.contains("fa-angle-down")) {
+                    $(this).addClass("fa-angle-up").removeClass("fa-angle-down");
+                    a.nextElementSibling.style.display = "block";
+                } else {
+                    $(this).addClass("fa-angle-down").removeClass("fa-angle-up");
+                    a.nextElementSibling.style.display = "none";
+                }
+            });
             $a.append($em).append($i);
             $li.append($a);
 
             // depth = 2
             var $subul;
-            a.childs && ($subul = $("<ul>").addClass("subul").appendTo($li).show()) && a.childs.forEach(function(b) {
+            a.childs && ($subul = $("<ul>").addClass("subul").appendTo($li)) && a.childs.forEach(function(b) {
                 var $subli = $("<li>").attr("data-id", b.id);
                 $subul.append($subli);
-                var $a = $("<a>").attr("data-id", b.id);  // addClass("roots") highlight
+                var $a = $("<a>").attr("data-id", b.id); // addClass("roots") highlight
                 $subli.append($a);
-                $em = $("<em>").addClass("iplus").appendTo($a);
+                $em = $("<em>").addClass("iless").appendTo($a);
                 var $i = b.childs ? $("<i>").attr("class", "fa iconfa fa-angle-down") : null;
+                $i && $i.on("click", function(e) {
+                    e.preventDefault();
+                    var t = this, $t = $(this),
+                        a = t.parentNode.parentNode;
+                    if (this.classList.contains("fa-angle-down")) {
+                        $t.addClass("fa-angle-up").removeClass("fa-angle-down");
+                        a.nextElementSibling.style.display = "block";
+                        $t.parent().prev().addClass("iplus").removeClass("iless");
+                    } else {
+                        $t.addClass("fa-angle-down").removeClass("fa-angle-up");
+                        a.nextElementSibling.style.display = "none";
+                        $t.parent().prev().addClass("iless").removeClass("iplus");
+                    }
+                });
                 $("<span>").text(b.name).append($i).appendTo($a);
 
                 // depth = 3
                 var $leaful;
-                b.childs && ($leaful = $("<ul>").addClass("subul").appendTo($subli).show()) && b.childs.forEach(function(c) {
+                b.childs && ($leaful = $("<ul>").addClass("subul").appendTo($subli)) && b.childs.forEach(function(c) {
                     var $leafli = $("<li>").attr("data-id", c.id);
                     $leaful.append($leafli);
                     var $a = $("<a>").attr("data-id", c.id).attr("href", c.href);
@@ -59,7 +86,16 @@ $(function() {
             });
         });
         // end data.childs.forEach
+
+        // click (.treeboxnew a) for highlight
+        var $alist = $box.find("a");
+        $alist.on("click", function(e) {
+            e.preventDefault();
+            var $t = $(this);
+            $alist.not($t).removeClass("roots");
+            $t.addClass("roots");
+            return false;
+        });
+
     })
 });
-
-
